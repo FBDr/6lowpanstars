aa1042b57e92a793a0860d211adcf88630269863
Floris <florisdrijver@live.nl>
2017-07-06 15:49:19 +0200

    Added python script to build execuatbles

---
+++ Create_Exe.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  parse.py
+#
+#  Copyright 2017 Floris <floris@ndn-icarus-simulator>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPsubprocessE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Bsubprocesston,
+#  MA 02110-1301, USA.
+#
+#
+
+import subprocess
+
+print "Pulling from GIT"
+subprocess.call(["git", "pull"])
+
+print "Compiling"
+subprocess.call("./waf")
+print "Building executable"
+subprocess.call(["./ErmineLightTrial.x86_64", "build/scratch/wsn-iot-v1", "--output", "static_exe"])
+print "Done!"
