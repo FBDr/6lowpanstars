4396a924eb7904e155927b7f15b770884a16eb88
Floris <florisdrijver@live.nl>
2017-10-03 17:27:45 +0200

    Battery update interval lowered to 1 per 3000s

--- scratch/wsn-iot-v1/wsn-iot-v1.cc
+++ scratch/wsn-iot-v1/wsn-iot-v1.cc
@@ -244,7 +244,7 @@ namespace ns3 {
                 mac[endN] = CreateObject<LrWpanContikiMac> ();
                 es[endN]->SetSupplyVoltage(3.3);
                 es[endN]->SetInitialEnergy(10800); //1 AA battery
-                es[endN]->SetEnergyUpdateInterval(Time(Seconds(100)));
+                es[endN]->SetEnergyUpdateInterval(Time(Seconds(3000)));
                 es[endN]->SetNode(iot[jdx].Get(idx));
                 es[endN]->AppendDeviceEnergyModel(em[endN]);
                 em[endN]->SetEnergySource(es[endN]);
